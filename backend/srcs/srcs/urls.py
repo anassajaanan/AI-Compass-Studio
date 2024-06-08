4aa68fee0c86ecc4aaa82629fd360028184645d7
tukka25 <aboodytukka@gmail.com>
2024-06-08 06:51:26 +0400

    videos added

--- backend/srcs/srcs/urls.py
+++ backend/srcs/srcs/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
 	path("", include("endpoint.urls")),
 ]
+
+# aboodytukka
+# etip1oHamMlrXgJz
+# mongodb+srv://aboodytukka:etip1oHamMlrXgJz@cluster0.dsxiqji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
